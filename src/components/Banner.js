e8b2346380902feed7c425225b21d81dab4f5c26
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-05-16 09:38:31 +0500

    Update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,45 +1,24 @@
 import React from "react";
 import "../scss/banner.scss";
 import { Link } from "react-router-dom";
-const Banner = ({ bg, text, isCenter, bc }) => {
+import first from "../assests/Photo.png"
+const Banner = () => {
   return (
     <>
       <div id="banner-wrapper">
         <section
           id="banner"
-          style={{
-            backgroundImage: `url(${bg})`,
-            backgroundSize: "cover",
-          }}
+
         >
-          <div className="conatiner-wrapper" style={{ background: bc }}>
-            <div
-              className="container"
-              style={
-                isCenter
-                  ? {
-                      textAlign: "center",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }
-                  : {}
-              }
-            >
-              <div className="heading" data-aos="fade-up">
-                <h1
-                  style={isCenter ? { textAlign: "center" } : {}}
-                  className={bc === "rgb(255 255 255 / 62%)" ? "grey" : null}
-                >
-                  {text}
-                </h1>
+          <div className="container__">
+            <div className="grid">
+              <div className="start">
+                <img src={first} alt="" />
               </div>
-              <div className="banner-btns" data-aos="fade-up">
-                <div className="btn">
-                  <Link to={"/Contact"}>Obtenir votre devis</Link>
-                </div>
-                <div className="btn">
-                  <a href="https://www.je-roule-en-electrique.fr" target="blank_"> Je passe à l'électrique</a>
-                </div>
+              <div className="end">
+                <h1>
+                  <h1>نصنع من</h1> رحلتكم وترحالكم شيئأ ممتعاً
+                </h1>
               </div>
             </div>
           </div>
