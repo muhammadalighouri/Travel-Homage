ed13c111b5719517de606cb7a6f28a0806f62d87
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-05-20 18:33:59 +0500

    update

--- src/components/Register.js
+++ src/components/Register.js
@@ -30,13 +30,13 @@ const Register = () => {
                 <div className="input">
                   <p>First name</p>
                   <div className="under">
-                  <input type="text" placeholder="First name" /> <img src={img1} alt="" /> 
+                    <input type="text" placeholder="First name" /> <img src={img1} alt="" />
                   </div>
                 </div>
                 <div className="input">
                   <p>Last name</p>
                   <div className="under">
-               <input type="text" placeholder="Last name" />    <img src={img1} alt="" /> 
+                    <input type="text" placeholder="Last name" />    <img src={img1} alt="" />
                   </div>
                 </div>
               </div>
@@ -50,19 +50,19 @@ const Register = () => {
               <div id="email" className="same">
                 <p>Email Adress</p>
                 <div className="under">
-                  <img src={img2} alt="" /><input type="email" name="" id="" placeholder="Example@domain.com"/> <img src={img1} alt="" />
+                  <img src={img2} alt="" /><input type="email" name="" id="" placeholder="Example@domain.com" /> <img src={img1} alt="" />
                 </div>
               </div>
               <div id="password" className="same">
                 <p>Password</p>
                 <div className="under">
-               <img src={img3} alt="" />   <input type="password" name="" id="" /> <img src={img5} alt="" />
+                  <img src={img3} alt="" />   <input type="password" name="" id="" /> <img src={img5} alt="" />
                 </div>
               </div>
               <div id="password" className="same">
                 <p>Confirm Password</p>
                 <div className="under">
-                <img src={img3} alt="" />   <input type="password" name="" id="" /> <img src={img4} alt="" />
+                  <img src={img3} alt="" />   <input type="password" name="" id="" /> <img src={img4} alt="" />
 
                 </div>
               </div>
