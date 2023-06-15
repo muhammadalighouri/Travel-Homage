6fa8b43ef54c6ab62f50189e03221148bdbf429c
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-06-15 11:42:30 -0700

    about

--- src/components/About.js
+++ src/components/About.js
@@ -5,31 +5,31 @@ import a33 from "../assests/About-img/a.33.png";
 const About = ({ mainImg, heading, img, des, order, end, top }) => {
   return (
     <>
-    <section id="about-wrapper">
-    <div
-        className="about-box"
-        style={order ? { gridTemplateColumns: "  2fr 1fr" } : {}}
-      >
+      <section id="about-wrapper">
         <div
-          id="img"
-          style={order ? { order: "2" } : {}}
-          className={top ? "top" : {}}
+          className="about-box"
+          style={order ? { gridTemplateColumns: "  2fr 1fr" } : {}}
         >
-          <img src={mainImg} alt=""   style={top ? {zIndex:'1'} : {}} />
-        </div>
-        <div className="content">
-          <div className="heading">
-            <img className="i" src={img} alt="" />
-            <h1>
-              {heading} <img src={a33} alt="" />
-            </h1>
+          <div
+            id="img"
+            style={order ? { order: "2" } : {}}
+            className={top ? "top" : {}}
+          >
+            <img src={mainImg} alt="" style={top ? { zIndex: "1" } : {}} />
           </div>
-          <div className="para">
-            <p style={end ? { textAlign: "end" } : {}}>{des}</p>
+          <div className="content">
+            <div className="heading">
+              <img className="i" src={img} alt="" />
+              <h1>
+                {heading} <img src={a33} alt="" />
+              </h1>
+            </div>
+            <div className="para">
+              <p style={end ? { textAlign: "end" } : {}}>{des}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
