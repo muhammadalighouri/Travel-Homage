7e61ed0708a3f29fe0992bbbeb3c1828291feda9
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-07-06 15:34:09 -0700

    upadte

--- src/Pages/CareerFormPage.js
+++ src/Pages/CareerFormPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import "../scss/careeers-page.scss";
+import "../scss/careers-form.scss";
 import { Input, Select } from "@material-ui/core";
 import Banner from "../components/Banner";
 import first from "../assests/Photo car.png";
@@ -9,10 +9,51 @@ import first from "../assests/Photo car.png";
 const CareerFormPage = () => {
   return (
     <>
-      <section id="career-page">
-        <Navigation nav={[]} />
-        <Banner text={"اسطولنا"} img={first} p={false} />{" "}
-        <div className="container"></div>
+
+<Navigation nav={[]} />
+  
+      <section id="career-form-page">
+           <div className="container">
+          <div className="heading">
+            <h1>وظائف يلو</h1>
+          </div>
+          <div className="content">
+            {[1, 2, 3, 4].map((e) => {
+              return (
+                <>
+                  <div className="grid-box">
+                    <div className="first">
+                      <div className="first-heading">
+                        <span>تم نشره في مارس 23, 2023</span>
+                        <p>(تم التحديث قبل واحد عام مضت)</p>
+                      </div>
+                      <a href="#" className="btn">
+                        قدِّم الآن
+                      </a>
+                    </div>
+                    <div className="second">
+                      <div class="second-heading-1">
+                        | السعودية | &nbsp;&nbsp; | &nbsp;&nbsp; رقم الوظيفة :
+                        <b class="black">Customer Care Agent</b>
+                      </div>
+                      <p>
+                        مسؤول رعاية عملاء
+                        <a href="#" target="blank_">
+                          ...اقرأ المزيد
+                        </a>
+                      </p>
+                      <div className="second-heading-2">
+                      الشواغر : <b>3</b> &nbsp; &nbsp; | &nbsp; &nbsp;التاريخ
+                      النهائي للتقديم : <b>31/12/2153</b>
+                    </div>
+                    </div>
+                   
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
       </section>
     </>
   );
