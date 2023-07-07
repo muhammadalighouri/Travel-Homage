4980514f3acd72e5109929383c7adbd2b9cd219f
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-07-07 07:00:46 -0700

    modal

--- src/Pages/CareersPage.js
+++ src/Pages/CareersPage.js
@@ -1,108 +1,56 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import "../scss/careeers-page.scss";
+import "../scss/careers-form.scss";
 import { Input, Select } from "@material-ui/core";
 import Banner from "../components/Banner";
-const CareersPage = () => {
+import first from "../assests/Photo car.png";
+import { Link } from "react-router-dom";
+
+const CareerPage = () => {
   return (
     <>
       <Navigation nav={[]} />
-      <Banner />
-      <section id="careers-page">
+      <Banner text={"وظائف"} img={first} p={true} />
+      <section id="career-form-page">
         <div className="container">
           <div className="heading">
-            <ul>
-              <li>
-                الرئيسية <span></span>
-              </li>
-              <li>
-                <a href="#">التوظيف</a>
-              </li>
-            </ul>
-            <h1>انضم الينا</h1>
+            <h1>وظائف يلو</h1>
           </div>
           <div className="content">
-            <div className="form-1">
-              <div className="form-heading">
-                <h1>المعلومات الشخصية</h1>
-              </div>
-
-              <div className="input-boxes">
-                <div className="input-box">
-                  <label htmlFor="">الهاتف</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor="">الاسم</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor="">الجنسية</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor="">البريد الإلكتروني</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor="">منطقة العمل</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor="">قسم</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor=""> الحالة الاجتماعية</label>
-                  <input type="text" />
-                </div>
-              </div>
-            </div>
-            <div className="form-1">
-              <div className="form-heading">
-                <h1>معلومات التعليم </h1>
-              </div>
-              <div className="input-boxes">
-                <div className="input-box">
-                  <label htmlFor=""> المدينة</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor=""> المستوى التعليمي</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor=""> سنة التخرج</label>
-                  <input type="text" />
-                </div>
-              </div>
-            </div>
-            <div className="form-1">
-              <div className="form-heading">
-                <h1>تحميل المستندات</h1>
-              </div>
-              <div className="input-boxes">
-                <div className="input-box">
-                  <label htmlFor=""> المدينة</label>
-                  <input type="text" />
-                </div>
-                <div className="input-box">
-                  <label htmlFor=""> المستوى التعليمي</label>
-                  <input type="text" />
-                </div>
-              </div>
-
-              <div className="check-btn">
-                <div className="check">
-                  أؤكد أن المعلومات التي تم إدخالها أعلاه صحيحة
-                  <input type="checkbox" name="" id="" />
-                </div>
-                <div className="btn">
-                  <button>إرسال</button>
-                </div>
-              </div>
-            </div>
+            {[1, 2, 3, 4].map((e) => {
+              return (
+                <>
+                  <div className="grid-box">
+                    <div className="first">
+                      <div className="first-heading">
+                        <span>تم نشره في مارس 23, 2023</span>
+                        <p>(تم التحديث قبل واحد عام مضت)</p>
+                      </div>
+                      <Link to={"/carrers-form"} className="btn">
+                        قدِّم الآن
+                      </Link>
+                    </div>
+                    <div className="second">
+                      <div class="second-heading-1">
+                        | السعودية | &nbsp;&nbsp; | &nbsp;&nbsp; رقم الوظيفة :
+                        <b class="black">Customer Care Agent</b>
+                      </div>
+                      <p>
+                        مسؤول رعاية عملاء
+                        <a href="#" target="blank_">
+                          ...اقرأ المزيد
+                        </a>
+                      </p>
+                      <div className="second-heading-2">
+                        الشواغر : <b>3</b> &nbsp; &nbsp; | &nbsp; &nbsp;التاريخ
+                        النهائي للتقديم : <b>31/12/2153</b>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </section>
@@ -111,4 +59,4 @@ const CareersPage = () => {
   );
 };
 
-export default CareersPage;
+export default CareerPage;
